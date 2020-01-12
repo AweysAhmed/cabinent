957d1739c973aa2a6843aead3303f7a6d1de5462
OFFICE <office@OFFICEs-MacBook-Air.local>
2020-01-12 00:07:03 -0500

    added gem files and created a controller and model

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class DocsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  # does not have a view
+  def create
+  end
+
+  def edit
+  end
+
+  # does not have a view
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_doc
+  end
+
+  def doc_params
+  end
+end
